f38b85b0b6b475fa79e78c11ce0ce56f423bd6f6
Pablo <pablobaiz12@gmail.com>
2023-07-12 23:56:22 -0300

    Add config to start deployment

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <main className='app_container'>
+      <div className='app'>
+        center main
+      </div>
+
+     </main>
+    </>
+  )
+}
+
+export default App
