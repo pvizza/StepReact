cf21b1eccf220f65a8bfd73453e3208e9ee7ff76
Pablo <pablobaiz12@gmail.com>
2023-07-19 23:32:59 -0300

    Add grid layout to sidenav

--- src/components/sideComponent/SideComponent.tsx
+++ src/components/sideComponent/SideComponent.tsx
@@ -1,9 +1,15 @@
 const SideComponent = () => {
   return (
-    <section style={{ flexGrow: 1 , backgroundColor: '#483eff', margin: '12px', borderRadius: '3%'}} className="side-component">
-      <div>
+    <section style={{ flexGrow: 1, backgroundColor: '#483eff', margin: '12px', borderRadius: '3%' }} className="side-component">
+    <div style={{ display: 'grid', gridTemplateColumns: 'auto', gridGap: '8px'}}>
+      <div style={{display: 'grid', gridTemplateColumns: '1fr 2fr'}}>
+        <div>
+        <p style={{alignSelf:'center', justifySelf:'center'}}>1</p>
+        </div>
+        <div>
         <span>step 1</span>
         <p>YOUR INFO</p>
+        </div>
       </div>
       <div>
         <span>step 2</span>
@@ -12,12 +18,13 @@ const SideComponent = () => {
       <div>
         <span>step 3</span>
         <p>ADD-ONS</p>
-        </div>
+      </div>
       <div>
         <span>step 4</span>
         <p>SUMMARY</p>
-        </div>
-    </section>
+      </div>
+    </div>
+  </section>
   );
 }
 
