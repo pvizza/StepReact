d06ded2b91ab405fbc58d65985ad57f1ff64399a
pvizza <pablobaizvizzari@gmail.com>
2023-07-25 00:09:21 -0300

    Add backgroud image to side component

--- src/components/sideComponent/SideComponent.tsx
+++ src/components/sideComponent/SideComponent.tsx
@@ -1,51 +1,39 @@
+import './SideComponent.css';
+
 const SideComponent = () => {
   return (
-    <section style={{ flexGrow: 1, backgroundColor: '#483eff', margin: '12px', borderRadius: '3%' }} className="side-component">
-    <div style={{ display: 'grid', gridTemplateColumns: 'auto', gridGap: '8px'}}>
-
-      <div style={{display: 'grid', gridTemplateColumns: '1fr 2fr',alignItems:'center'}}>
-        <div>
-        <p style={{alignSelf:'center', justifySelf:'center',textAlign:'center'}}>1</p>
+    <section className="side-component">
+      <div className="grid-container">
+        <div className="grid-item">
+          <p className="step-number step-number-circle">1</p>
+          <div>
+            <span>step 1</span>
+            <p>YOUR INFO</p>
+          </div>
         </div>
-
-        <div>
-        <span>step 1</span>
-        <p>YOUR INFO</p>
+        <div className="grid-item">
+          <p className="step-number step-number-circle">2</p>
+          <div>
+            <span>step 2</span>
+            <p>SELECT PLAN</p>
+          </div>
         </div>
-      </div>
-      <div style={{display: 'grid', gridTemplateColumns: '1fr 2fr',alignItems:'center'}}>
-        <div style={{textAlign:'center'}}>
-        <p style={{alignSelf:'center', justifySelf:'center',textAlign:'center', display: 'inline-block', width: '30px', height: '30px', borderRadius: '50%', backgroundColor: 'red'}}>2</p>
+        <div className="grid-item">
+          <p className="step-number step-number-circle">3</p>
+          <div>
+            <span>step 3</span>
+            <p>ADD ONSs</p>
+          </div>
         </div>
-
-        <div>
-        <span>step 2</span>
-        <p>SELECT PLAN</p>
-        </div>
-      </div>
-      <div style={{display: 'grid', gridTemplateColumns: '1fr 2fr',alignItems:'center'}}>
-        <div>
-        <p style={{alignSelf:'center', justifySelf:'center',textAlign:'center'}}>3</p>
-        </div>
-
-        <div>
-        <span>step 3</span>
-        <p>ADD ONSs</p>
+        <div className="grid-item">
+          <p className="step-number step-number-circle">4</p>
+          <div>
+            <span>step 4</span>
+            <p>SUMMARY</p>
+          </div>
         </div>
       </div>
-      <div style={{display: 'grid', gridTemplateColumns: '1fr 2fr',alignItems:'center'}}>
-        <div>
-        <p style={{alignSelf:'center', justifySelf:'center',textAlign:'center'}}>4</p>
-        </div>
-
-        <div>
-        <span>step 4</span>
-        <p>SUMMARY</p>
-        </div>
-      </div>
-      
-    </div>
-  </section>
+    </section>
   );
 }
 
