fdc5d5b639c5247583fef254f7a158e3932ed1d2
pvizza <pablobaizvizzari@gmail.com>
2023-09-27 22:16:28 -0300

    Feat: add responsive sidebar

--- src/components/sideComponent/SideComponent.tsx
+++ src/components/sideComponent/SideComponent.tsx
@@ -8,28 +8,28 @@ const SideComponent = ({currentStep}:SideComponentProps) => {
       <div className="grid-container">
         <div className="grid-item">
           <p className={currentStep === 1 ? "step-number step-number-circle active" : "step-number step-number-circle"}>1</p>
-          <div>
+          <div className='step-container'>
             <span className='step-title'>STEP 1</span>
             <p className='step-description'>YOUR INFO</p>
           </div>
         </div>
         <div className="grid-item">
           <p className={currentStep === 2 ? "step-number step-number-circle active" : "step-number step-number-circle"}>2</p>
-          <div>
+          <div className='step-container'>
             <span className='step-title'>STEP 2</span>
             <p className='step-description'>SELECT PLAN</p>
           </div>
         </div>
         <div className="grid-item">
           <p  className={currentStep === 3 ? "step-number step-number-circle active" : "step-number step-number-circle"}>3</p>
-          <div>
+          <div className='step-container'>
             <span className='step-title'>STEP 3</span>
             <p className='step-description'>ADD-ONS</p>
           </div>
         </div>
         <div className="grid-item">
           <p className={currentStep === 4 || currentStep === 5 ? "step-number step-number-circle active" : "step-number step-number-circle"}>4</p>
-          <div>
+          <div className='step-container'>
             <span className='step-title'>STEP 4</span>
             <p className='step-description'>SUMMARY</p>
           </div>
